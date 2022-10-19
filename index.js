30d655d38c556d7643d24df59579c6e4d1f09bba
Anastasiia Lymarenko <anastasiialimarenko@gmail.com>
2022-10-19 20:51:14 +0300

    Assign

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-console.log(33);
-alert('Hello, world');
+/* console.log(true);
+console.log('This is text');
+console.log(50);
+console.log(undefined);
+console.log(null);
+console.log(5n); */
+
+/*
+Змінна
+Variable
+Cтворення змінної - 
+ключове_слово ім'я_змінної
+
+var -- deprecated
+
+let - CD-RW(можна перезаписувать)
+const - CD-R(можна тільки записать)
+*/
+
+let box; // оголошення змінної (variable declaration)
+box = 3;
+console.log(box);
+/*Оператор присвоєння = */
+// box = 3; //Присвоєння (Assign)
+let box2 = 'Hello';
+
+
+box2 = 10;
+console.log(box2);
