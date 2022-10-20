cdb5a6329ff06f28cd719f0d7936308752a0de5e
Anastasiia Lymarenko <anastasiialimarenko@gmail.com>
2022-10-20 18:42:53 +0300

    const

--- index.js
+++ index.js
@@ -1,49 +1,12 @@
-/* console.log(true);
-console.log('This is text');
-console.log(50);
-console.log(undefined);
-console.log(null);
-console.log(5n); */
+let box1 = 'Hello';
 
-/*
-Змінна
-Variable
-Cтворення змінної - 
-ключове_слово ім'я_змінної
+const box2 = true;
 
-var -- deprecated
+// console.log(box1);
+// console.log(box2);
 
-let - CD-RW(можна перезаписувать)
-const - CD-R(можна тільки записать)
-*/
+box1 = 10;
+console.log(box1);
 
-let box; // оголошення змінної (variable declaration)
-box = 3;
-console.log(box);
-/*Оператор присвоєння = */
-// box = 3; //Присвоєння (Assign)
-let box2 = 'Hello';
-
-
-box2 = 10;
+box2 = 15;
 console.log(box2);
-
-/* 
-Правила уменування
-CamelCase
-ім'я змінної повинно бути таким щоб було зрозуміло що в ній зберігається
-
-let data; - bad practice
-let value; - bad practice
-
-let 123value; - bad practice назва змінної повинна починатися з букви
-
-let _value; - bad practice. "_" - приватне поле
-let #value; - bad practice. "#" - приватне поле
-
-*/
-
-let importantVariable;
-importantVariable = 0;
-
-let a = 6;
