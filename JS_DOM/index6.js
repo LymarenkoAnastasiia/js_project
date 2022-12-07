bd69ad7eda4f57145868dc9571855910a998df1d
Anastasiia Lymarenko <anastasiialimarenko@gmail.com>
2022-12-07 17:34:52 +0200

    Monobank API

---
+++ JS_DOM/index6.js
@@ -0,0 +1,29 @@
+fetch('https://api.monobank.ua/bank/currency')
+.then((response) => response.json())
+.then((data) => {replaceCurrencyCode(data)})
+
+function replaceCurrencyCode(courseData) {
+fetch('https://pkgstore.datahub.io/core/currency-codes/codes-all_json/data/029be9faf6547aba93d64384f7444774/codes-all_json.json')
+.then((response) => response.json())
+.then((data) => {
+    const mapWithCodes = new Map();
+    data.map((item) => {mapWithCodes.set(item.NumericCode, item.Currency)})
+
+    courseData.forEach((currentCurrency) => {
+        currentCurrency.currencyCodeA = mapWithCodes.get(currentCurrency.currencyCodeA);
+        currentCurrency.currencyCodeB = mapWithCodes.get(currentCurrency.currencyCodeS);
+    })
+    showCource(courseData);
+})
+}
+
+function showCource(courseData) {
+    courseData.map((item) => {
+        if(item.rateBuy && item.rateSell) {
+        const div = document.createElement('div');
+        div.append(`Монобанк продает ${item.currencyCodeA} за ${item.rateSell} ${item.currencyCodeB}, а покупает за ${item.rateBuy} ${item.currencyCodeB}`);
+
+        document.body.append(div);
+    }
+    })
+}
